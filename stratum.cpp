84e5cad7f96573ae3d1719cb73ffd31255171e35
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2020-12-24 13:55:08 +0100

    add balloon

--- stratum.cpp
+++ stratum.cpp
@@ -117,6 +117,7 @@ YAAMP_ALGO g_algos[] =
 	{"argon2d500", argon2d_dyn_hash, 0x10000, 0, 0 }, // Dynamic Argon2d Implementation
 	{"argon2d4096", argon2d_uis_hash, 0x10000, 0, 0 }, // Argon2d Implementation
 	{"astralhash", astralhash_hash, 0x100, 0, 0},
+	{"balloon", balloon, 0x100, 0, 0},
 	{"bastion", bastion_hash, 1, 0 },
 	{"bcd", bcd_hash, 1, 0, 0},
 	{"bitcore", timetravel10_hash, 0x100, 0, 0},
